6c17aeb4125eac816a34aa13d1a57c3d65744676
Justin Gottschalk <justindevs@gmail.com>
2023-12-13 21:14:22 -0600

    App working with sample code guts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,25 +1,10 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
     res.render('homepage', {
-      projects,
       logged_in: req.session.logged_in
     });
   } catch (err) {
@@ -32,8 +17,7 @@ router.get('/profile', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
+      attributes: { exclude: ['password'] }
     });
 
     const user = userData.get({ plain: true });
