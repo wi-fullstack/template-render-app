1fff2f34ad9da244791ac803345c4aa47a5d4f42
Justin Gottschalk <justindevs@gmail.com>
2023-12-18 18:21:56 -0600

    J/add app routes (#2)
    * Added app html routes
    
    * fixed auth on routes and other stuff

--- controllers/app.routes.js
+++ controllers/app.routes.js
@@ -2,56 +2,29 @@ const router = require('express').Router();
 const withAuth = require('../utils/auth');
 
 router.get('/product/:id', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
-
-  res.render('product');
+  res.render('product', {
+    productId: req.params.id
+  });
 });
 
-router.get('/product/create', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
-
+router.get('/product/create', withAuth,(req, res) => {
   res.render('create-product');
 });
 
-router.get('/product/edit/:id', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
+router.get('/product/edit/:id', withAuth,(req, res) => {
 
   res.render('create-product', {
     productId: req.params.id
   });
 });
 
-router.get('/category/create', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
-
+router.get('/category/create', withAuth,(req, res) => {
   res.render('create-category');
 });
 
-router.get('/category/edit/:id', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-    return;
-  }
-
+router.get('/category/edit/:id', withAuth,(req, res) => {
   res.render('create-category', {
-    productId: req.params.id
+    categoryId: req.params.id
   });
 });
 
